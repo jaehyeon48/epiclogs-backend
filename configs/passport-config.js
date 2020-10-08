fcad598a7991d89321e4459f494fb9a457c5a5f7
jaehyeon48 <imjaehyeon48@gmail.com>
2020-10-08 13:19:19 +0900

    modified google auth's redirect url

--- configs/passport-config.js
+++ configs/passport-config.js
@@ -23,7 +23,7 @@ passport.deserializeUser(async (userId, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5000/api/auth/login/google/credential'
+  callbackURL: 'https://epiclogs.herokuapp.com/api/auth/login/google/credential'
 }, async (accessToken, refreshToken, profile, done) => {
   // retrieve user data from 'profile' response
   const firstName = profile['_json']['given_name'];
