28e460a25f1012e2929557bc10617b22c3033731
jaehyeon48 <imjaehyeon48@gmail.com>
2020-10-08 13:39:59 +0900

    modified redirect url in makeTokenForGoogleAuth controller

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -42,7 +42,8 @@ function makeTokenForGoogleAuth(req, res) {
     jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '12h' }, (err, token) => { // set expiresIn 12h for testing purpose.
       if (err) throw err;
       res.cookie('token', token, { httpOnly: true });
-      res.redirect('http://localhost:3000');
+      // redirect to provided url after google auth successfully processed
+      res.redirect('https://epiclogs.tk');
     });
   } catch (error) {
     console.log(error);
