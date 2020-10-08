c0a5b6f1928d00f447783ea9db7bd44a37f184c2
jaehyeon48 <imjaehyeon48@gmail.com>
2020-10-08 14:37:07 +0900

    redirection test for github auth

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -64,7 +64,8 @@ async function makeTokenForGithubAuth(req, res) {
     jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '12h' }, (err, token) => { // set expiresIn 12h for testing purpose.
       if (err) throw err;
       res.cookie('token', token, { httpOnly: true });
-      res.redirect('http://localhost:3000');
+      // redirect to provided url after github auth successfully processed
+      res.redirect('https://epiclogs.tk');
     });
   } catch (error) {
     console.log(error);
