720f76fb27e81e9be7924510ee9e315c3e1388cd
jaehyeon48 <imjaehyeon48@gmail.com>
2020-11-02 11:01:03 +0900

    oauth redirection test 4

--- middlewares/google-auth-middleware.js
+++ middlewares/google-auth-middleware.js
@@ -51,6 +51,7 @@ async function googleAuthMiddleware(req, res, next) {
 
         // req.googleUserId = newUserId.insertId;
         // next();
+        console.log(newUserId.insertId);
         const encryptedNewUserId = await bcrypt.hash(newUserId.insertId, 10);
         return res.redirect(301, `https://epiclogs.tk/auth/n-name?u=${encryptedNewUserId}`);
       }
